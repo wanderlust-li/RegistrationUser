3a0a9575a3a36c803f5e4cf15033e491c101ba9e
YevheniiLichman <zhekalichm4n@icloud.com>
2023-02-27 13:38:32 +0200

    One Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RegistrationUser.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
